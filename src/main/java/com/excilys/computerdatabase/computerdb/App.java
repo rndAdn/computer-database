5fe9fbec636b33796733bac03948943b95f43b9e
Renaud Adequin <radequin@excilys.com>
2017-02-20 10:10:00 +0100

    init maven project

---
+++ src/main/java/com/excilys/computerdatabase/computerdb/App.java
@@ -0,0 +1,11 @@
+package com.excilys.computerdatabase.computerdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
