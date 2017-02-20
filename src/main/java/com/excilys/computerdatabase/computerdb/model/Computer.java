980a43303f0712bb925d747f8d9389b3017d23f9
Renaud Adequin <radequin@excilys.com>
2017-02-20 11:29:17 +0100

    add ComputerModel

---
+++ src/main/java/com/excilys/computerdatabase/computerdb/model/Computer.java
@@ -0,0 +1,48 @@
+package com.excilys.computerdatabase.computerdb.model;
+
+import java.util.Calendar;
+
+public class Computer {
+
+	
+	
+	private int id;
+	private String name;
+	private Calendar dateIntroduced;
+	private Calendar dateDiscontinued;
+	private int compagnyId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Calendar getDateIntroduced() {
+		return dateIntroduced;
+	}
+	public void setDateIntroduced(Calendar dateIntroduced) {
+		this.dateIntroduced = dateIntroduced;
+	}
+	public Calendar getDateDiscontinued() {
+		return dateDiscontinued;
+	}
+	public void setDateDiscontinued(Calendar dateDiscontinued) {
+		this.dateDiscontinued = dateDiscontinued;
+	}
+	public int getCompagnyId() {
+		return compagnyId;
+	}
+	public void setCompagnyId(int compagnyId) {
+		this.compagnyId = compagnyId;
+	}
+	
+	
+	
+	
+}
