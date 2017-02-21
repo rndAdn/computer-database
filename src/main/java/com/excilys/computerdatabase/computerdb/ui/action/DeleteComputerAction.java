bd3b319f28fdd2690672ef091a0fa98e082165a4
Renaud Adequin <radequin@excilys.com>
2017-02-21 11:33:01 +0100

    add catch NumberFormatException

--- src/main/java/com/excilys/computerdatabase/computerdb/ui/action/DeleteComputerAction.java
+++ src/main/java/com/excilys/computerdatabase/computerdb/ui/action/DeleteComputerAction.java
@@ -13,25 +13,34 @@ public class DeleteComputerAction implements ActionMenu{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Entrez l'id de l'ordinateur à supprimer : ");
 		String s  = sc.nextLine();
-		int id = Integer.parseInt(s); // TODO : Exception Number_Format & Not_A_Number
 		
-		Computer computer = Database.getComputerDao().getComputerById(id);
 	    
 		try{
+			int id = Integer.parseInt(s);
+			
+			Computer computer = Database.getComputerDao().getComputerById(id);
+			
 			System.out.println(computer.getDetail());
 			System.out.print("Supprimer ? [O/n]");
+			
 			String reponse  = sc.nextLine();
 			System.out.println();
+			
 			if(reponse.equalsIgnoreCase("n")){
 				System.out.print("Ordinateur non supprimé");
 				return; 
 			}
+			
 			Database.getComputerDao().deleteComputer(computer);
 			System.out.print("Ordinateur supprimé");
 		}
 		catch (NullPointerException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 			System.out.println("Aucun Ordinateur ne correspond à cette ID");
+		}
+		catch (NumberFormatException e) {
+			//e.printStackTrace();
+			System.out.println("id Incorrect");
 		}
 		finally {
 			System.out.println();
