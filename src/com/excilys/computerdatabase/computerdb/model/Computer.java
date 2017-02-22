c295b58b0a9e994ed722772955d794ae31aebdf7
Renaud Adequin <radequin@excilys.com>
2017-02-22 17:45:11 +0100

    implement equals & hashcode

--- src/com/excilys/computerdatabase/computerdb/model/Computer.java
+++ src/com/excilys/computerdatabase/computerdb/model/Computer.java
@@ -66,10 +66,59 @@ public class Computer implements Pageable {
 				"\tDate fin : " + ((dateDiscontinued != null)?dateDiscontinued:"NC") +
 				"\tCompany : " + ((company!= null)?company.getName():"NC"); 
 	}
-	
-	
-	public void checkComputer(){
-		
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((dateDiscontinued == null) ? 0 : dateDiscontinued.hashCode());
+		result = prime * result + ((dateIntroduced == null) ? 0 : dateIntroduced.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (dateDiscontinued == null) {
+			if (other.dateDiscontinued != null)
+				return false;
+		} else if (!dateDiscontinued.equals(other.dateDiscontinued))
+			return false;
+		if (dateIntroduced == null) {
+			if (other.dateIntroduced != null)
+				return false;
+		} else if (!dateIntroduced.equals(other.dateIntroduced))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
