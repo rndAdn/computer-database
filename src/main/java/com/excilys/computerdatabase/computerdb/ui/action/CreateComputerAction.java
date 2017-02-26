44051a1fcdeb18edc9a22b03f608f46fe3dfc4c0
Adequin Renaud <adequin.renaud@gmail.com>
2017-02-26 15:34:49 +0100

    change check order in CreateComputerAction

--- src/main/java/com/excilys/computerdatabase/computerdb/ui/action/CreateComputerAction.java
+++ src/main/java/com/excilys/computerdatabase/computerdb/ui/action/CreateComputerAction.java
@@ -22,21 +22,17 @@ public class CreateComputerAction implements ActionMenu {
 		System.out.print("Entrez le nom de l'ordinateur (Obligatoire): ");
 		String name  = sc.nextLine();
 		
-		System.out.print("Entrez la date d'introdution (jj-mm-aaaa): ");
-		String dateIntroString = sc.nextLine();
-		
-		System.out.print("Entrez la date de fin de service (jj-mm-aaaa) : ");
-		String dateFinServiceString = sc.nextLine();
-		
-		System.out.print("Entrez l'id de la companie : ");
-		String companyIdString = sc.nextLine();
-		
-		
 		boolean blankName = StringUtils.isBlank(name);
 		if(blankName){
 			System.out.println("Le nom de l'ordinateur n'est pas valide");
 			return;		
 		}
+		
+		System.out.print("Entrez la date d'introdution (jj-mm-aaaa): ");
+		String dateIntroString = sc.nextLine();
+		
+		System.out.print("Entrez la date de fin de service (jj-mm-aaaa) : ");
+		String dateFinServiceString = sc.nextLine();
 		
 		Optional<LocalDate> dateIntro = Utils.stringToDate(dateIntroString);
 		Optional<LocalDate> dateFin = Utils.stringToDate(dateFinServiceString);
@@ -48,11 +44,13 @@ public class CreateComputerAction implements ActionMenu {
 			return;
 		}
 		
-		Optional<Company> optionalCompany = Optional.empty();
+		System.out.print("Entrez l'id de la companie : ");
+		String companyIdString = sc.nextLine();
 		
+		
+		Optional<Company> optionalCompany = Optional.empty();	
 		if(!StringUtils.isBlank(companyIdString)){
 			long companyid = Utils.stringToId(companyIdString);
-			
 			optionalCompany = CompanyService.getCompanyByid(companyid);
 		}
 		
@@ -68,6 +66,8 @@ public class CreateComputerAction implements ActionMenu {
 		System.out.print("Ajouter ? [O/n]");
 		String reponse  = sc.nextLine();
 		System.out.println();
+		
+		
 		if(reponse.equalsIgnoreCase("n")){
 			System.out.print("Ordinateur non ajouté");
 			return; 
