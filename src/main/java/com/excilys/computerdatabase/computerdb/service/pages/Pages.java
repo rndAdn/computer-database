c4468dd23d93329789931e01ae648f90c3556140
Renaud Adequin <radequin@excilys.com>
2017-03-03 12:13:56 +0100

    [feature] Service Use CountAllFilterByName

---
+++ src/main/java/com/excilys/computerdatabase/computerdb/service/pages/Pages.java
@@ -0,0 +1,120 @@
+package com.excilys.computerdatabase.computerdb.service.pages;
+
+import java.util.List;
+
+public abstract class Pages {
+    
+    protected long rowByPages = 30;
+    protected long pageNumber = 1;
+    protected long totalRow;
+    protected String filter;
+    
+    /**
+     * Get item List of current page.
+     *
+     * @return Pageable List.
+     */
+    public abstract List<Pageable> getItems();
+    
+    /**
+     * Get the current page.
+     *
+     * @return The current page
+     */
+    public Page getCurrentPage() {
+        List<Pageable> pageables = getItems();
+        return new Page(pageables);
+    }
+    
+    public void resetFilter(){
+        filter = "";
+    }
+    
+    public void setFilter(String pattern){
+        this.filter = pattern;
+    }
+    
+    public String getFilter(){
+        return filter;
+    }
+    
+    /**
+     * Switch to next Page.
+     *
+     */
+    public void nextPage() {
+        pageNumber = Math.min(getTotalNumberOfPage(), pageNumber + 1);
+    }
+
+    /**
+     * Switch to previous Page.
+     *
+     */
+    public void previousPage() {
+        pageNumber = Math.max(1, pageNumber - 1);
+    }
+
+    /**
+     * Switch to first Page.
+     *
+     */
+    public void firstPage() {
+        pageNumber = 1;
+    }
+
+    /**
+     * Switch to Last Page.
+     *
+     */
+    public void lastPage() {
+        pageNumber = getTotalNumberOfPage();
+    }
+    
+    /**
+     * Get the current page index.
+     *
+     * @return page Index
+     */
+    public long getPageIndex() {
+        return pageNumber;
+    }
+
+    /**
+     * Set the current page index bound between 1 and totalNumberOfpages.
+     *
+     * @param pageNumber
+     *            .
+     */
+    public void setPageIndex(long pageNumber) {
+        this.pageNumber = Math.max(Math.min(getTotalNumberOfPage(), pageNumber), 1);
+    }
+    
+    /**
+     * Set the limit of item by page and set the number of page.
+     *
+     * @param limit
+     *            of row on each page
+     */
+    public void setRowByPages(long limit) {
+        this.rowByPages = limit;
+    }
+    
+    public long getTotalRow() {
+        return totalRow;
+    }
+    
+    public long getTotalNumberOfPage() {
+        return (long) Math.ceil(totalRow / (double) rowByPages);
+    }
+    
+    /**
+     * Set the total number of item to display.
+     *
+     * @param nbRow
+     *            .
+     */
+    public void setTotalNumberOfRow(long nbRow) {
+        this.totalRow = nbRow;
+    }
+    
+}
--- src/main/java/com/excilys/computerdatabase/computerdb/service/pages/PagesListComputer.java
+++ src/main/java/com/excilys/computerdatabase/computerdb/service/pages/PagesListComputer.java
@@ -3,31 +3,29 @@ package com.excilys.computerdatabase.computerdb.service.pages;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.excilys.computerdatabase.computerdb.database.ComputerDao;
 import com.excilys.computerdatabase.computerdb.database.DaoException;
 
-public class PagesListComputer extends PagesList {
+public class PagesListComputer extends Pages {
 
     @Override
-    public List<Pageable> getList() {
+    public List<Pageable> getItems() {
         List<Pageable> list = new ArrayList<>();
         try {
-            list = ComputerDao.INSTANCE.getComputers((pageNumber - 1) * rowByPages, rowByPages);
+            if (StringUtils.isBlank(filter)) {
+                list = ComputerDao.INSTANCE.getComputers((pageNumber - 1) * rowByPages, rowByPages);
+                totalRow = ComputerDao.INSTANCE.countComputers();
+            } else {
+                list = ComputerDao.INSTANCE.getComputersByName(filter, (pageNumber - 1) * rowByPages, rowByPages);
+                totalRow = ComputerDao.INSTANCE.countComputersWithName(filter);
+            }
         } catch (DaoException e) {
             e.printStackTrace();
         }
         return list;
+        
+        
     }
-
-    @Override
-    public List<Pageable> getListFilterByName(String name) {
-        List<Pageable> list = new ArrayList<>();
-        try {
-            list = ComputerDao.INSTANCE.getComputersByName(name, (pageNumber - 1) * rowByPages, rowByPages);
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
 }
