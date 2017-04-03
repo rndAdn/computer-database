e7ea36548ce342932a6423823f4f0ea77be91b69
Renaud Adequin <radequin@excilys.com>
2017-04-03 10:09:13 +0200

    [FEATURE] add computer rowMapper

--- src/main/java/com/excilys/computerdatabase/computerdb/dao/mapper/MapperComputer.java
+++ src/main/java/com/excilys/computerdatabase/computerdb/dao/mapper/MapperComputer.java
@@ -4,10 +4,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.excilys.computerdatabase.computerdb.dao.ComputerDao;
 import com.excilys.computerdatabase.computerdb.model.entities.Company;
 import com.excilys.computerdatabase.computerdb.model.entities.Computer;
 
-public class MapperComputer {
+public class MapperComputer implements RowMapper{
 
 
 
@@ -38,6 +41,23 @@ public class MapperComputer {
         }
         Computer computer = new Computer.ComputerBuilder(name).id(id).dateIntroduced(intro).dateDiscontinued(fin)
                 .company(company).build();
+        return computer;
+    }
+
+    @Override
+    public Object mapRow(ResultSet rs, int arg1) throws SQLException {
+        long companyId = rs.getLong("company_id");
+        Company company = null;
+        if (!rs.wasNull()) {
+            String nameComapny = rs.getString("company_name");
+            company = new Company.CompanyBuilder(nameComapny).id(companyId).build();
+        }
+        Computer computer = new Computer.ComputerBuilder(rs.getString("name"))
+                .id(rs.getLong("id"))
+                .dateIntroduced(rs.getString("introduced"))
+                .dateDiscontinued(rs.getString("discontinued"))
+                .company(company)
+                .build();
         return computer;
     }
 
